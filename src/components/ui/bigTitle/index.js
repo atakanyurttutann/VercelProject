8e98c5a48fc0253be7d2b3aef21cb8ea29780688
atakanyurttutann <76009848+atakanyurttutann@users.noreply.github.com>
2021-10-31 13:28:57 +0300

    font size fixed

--- src/components/ui/bigTitle/index.js
+++ src/components/ui/bigTitle/index.js
@@ -1,7 +1,7 @@
 const BigTitle = ({title,classParameter})=>{
  return(
    
-         <div className={`flex flex-row justify-center items-center text-5xl bg-black text-white pt-10 pb-10 font-bold ${classParameter}`}>
+         <div className={`flex flex-row justify-center items-center xl:text-[50px]  text-[35px] bg-black text-white pt-10 pb-10 font-bold ${classParameter}`}>
         {title}
       </div>
      
